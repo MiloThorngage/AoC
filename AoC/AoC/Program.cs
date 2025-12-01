9cd7c16669a55b28c1a57a932e19019bba059836
MiloThorngage <23049820@student.hhs.nl>
2025-12-01 21:30:59 +0100

    D1P2

--- AoC/AoC/Program.cs
+++ AoC/AoC/Program.cs
@@ -7,26 +7,44 @@ void addValue(string input)
 {
     
     string direction = input.Substring(0, 1);
-    int newValue = value;
-    newValue = int.Parse(input.Substring(1, input.Length - 1));
-    Console.WriteLine($"Direction: {direction}, Value: {newValue}");
-    
+    int distance = int.Parse(input.Substring(1, input.Length - 1));
+    Console.WriteLine($"Direction: {direction}, Distance: {distance}, Start: {value}");
+
+    int wraps = 0;
+    int newValue;
+
     if (direction == "R")
     {
-        newValue = value + newValue;
-        while(newValue > 99){
-            newValue = newValue - 100;
+        // First time we hit 0 when rotating right is after (100 - value) clicks,
+        // except when starting at 0 where that first hit is after 100 clicks.
+        int firstHit = (value == 0) ? 100 : (100 - value);
+        if (distance >= firstHit)
+        {
+            wraps = 1 + (distance - firstHit) / 100;
         }
-       
-        
+
+        newValue = (value + distance) % 100;
     }
-    else if (direction == "L")
+    else // "L"
     {
-        newValue = value - newValue;
-       while(newValue < 0){
-            newValue = (newValue + 100);
+        // First time we hit 0 when rotating left is after value clicks,
+        // except when starting at 0 where that first hit is after 100 clicks.
+        int firstHit = (value == 0) ? 100 : value;
+        if (distance >= firstHit)
+        {
+            wraps = 1 + (distance - firstHit) / 100;
         }
+
+        // C# % of negative can be negative, normalize to 0..99
+        newValue = ((value - distance) % 100 + 100) % 100;
     }
+
+    if (wraps > 0)
+    {
+        Console.WriteLine($"Wrapped around {wraps} time(s)");
+        zeroCount += wraps;
+    }
+
     Console.WriteLine("Current Value: " + newValue);
     value = newValue;
 
@@ -36,17 +54,14 @@ void addValue(string input)
 try
 {
     // Read all lines into a string array (one array element per line)
-    string[] lines = File.ReadAllLines("input.txt");
+    string[] lines = File.ReadAllLines("Input.txt");
 
-    Console.WriteLine($"Read {lines.Length} lines from input.txt:");
+    Console.WriteLine($"Read {lines.Length} lines from Input.txt:");
     for (int i = 0; i < lines.Length; i++)
     {
         addValue(lines[i]);
-        if(value == 0)
-        {
-            zeroCount++;
-
-        }
+        Console.WriteLine($"CurrentCount: {zeroCount}");
+        
     }
 }
 catch (IOException e)
